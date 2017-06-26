931206529808f7719572287aa72824b7416278fa
Pelatiah Ishmael Morgan <Ish@Pelatiahs-MacBook.local>
2017-06-26 11:33:13 -0500

    commit

---
+++ jquery.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    /*
+	$("#keyboard-upper-container").fadeOut(1000);
+	*/
+	$('#keyboard-upper-container').on('keydown', function() {
+		$('#keyboard-upper-container').slideToggle(500);
+	});
+});
+/*
+	$(document).keydown(function(event) {
+		if(event.which = 16) {
+			$("#keyboard-upper-container").hide();
+			$("#keyboard-lower-container").show();
+		}
+
+	});
+
+	$(document).keyup(function(event) {
+		if(event.which = 16) {
+			$("#keyboard-upper-container").hide();
+			$("#keyboard-lower-container").show();
+		}
+	
+	});
+
+    $("#keyboard-lower-container").click(function(){
+        $("#keyboard-lower-container").keypress();
+
+$(document).on("keypress", function() {
+	$('#' + event.which).css(background-color: "Lightblue"));
+	$(document).keyup(function() {
+		$('#' + event.which).css(background-color: "#f5f5f5")) 
+	})
+
+});
+
+var sentence = [];
+for (var i=0
+console.log(sentence[i]);
+    
+});
+*/
+
+function config package() {
+
+}
