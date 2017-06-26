99f66a14dde9a51348d00a1828cf83223491bec6
InnovationBham_4 <InnovationBham_4@Admins-iMac.local>
2017-06-26 17:21:48 -0500

    type

--- jquery.js
+++ jquery.js
@@ -1,63 +1,49 @@
-// $(document).ready(function() {
-// 	$('#keyboard-upper-container').hide();
-// });
-
-// $(document).keydown(function(event) {
-// 		if(event.which == 16) {
-// 			$("#keyboard-lower-container").hide();
-// 			$("#keyboard-upper-container").show();
-// 		}
-
-// });
-
-// $(document).keyup(function(event) {
-// 		if(event.which == 16) {
-// 			$("#keyboard-upper-container").hide();
-// 			$("#keyboard-lower-container").show();
-// 		};
-// });
-	
-// $(document).on("keypress", function(event){
-// 		$("#" + event.which).css({backgroundColor: "Lightblue"});
-// 		$(document).keyup(function(){ 
-// 			$("#" + event.which).css({backgroundColor: "#f5f5f5"});
-// 		})
-// });
-
 var sentences = [
-	'ten ate neite ate nee enet ite ate inet ent eate'
-];
+	'Raindrop', 
+	'(DRIP)', 'DROP TOP', 'eating some cookies in a hotbox'];
 
-$("#sentence").append(sentences[0]);
+sentenceCounter = 0;
+letterCounter = 0;
+currentsentence = '';
 
+$(document).keydown(function(event) {
+	$( "#yellow-block" ).animate({
+			left: "+=50",
+		},  function() {
+			// Animation complete.
+		});
+});
+$(document).ready(function() {
+	$('#keyboard-upper-container').hide();
 
+	$("#sentence").append(sentences[0]);
+});
 
+$(document).keydown(function(event) {
+		if(event.which == 16) {
+			$("#keyboard-lower-container").hide();
+			$("#keyboard-upper-container").show();
+		}
 
+});
 
+$(document).keyup(function(event) {
+		if(event.which == 16) {
+			$("#keyboard-upper-container").hide();
+			$("#keyboard-lower-container").show();
+		};
+});
+	
+$(document).on("keypress", function(event){
+		$("#" + event.which).css({backgroundColor: "Lightblue"});
+		$(document).keyup(function(){ 
+			$("#" + event.which).css({backgroundColor: "#f5f5f5"});
+		})
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var a =
-var b =
-var c =
-var d =
-var e =
-$("#sentence").append(a);
-*/
+function handleSentences() {
+	$('#sentences').empty();
+	currentsentence = sentences
+	$('#sentences').append(sentences[sentencesCounter]);
+	sentenesCounter++;
+}
